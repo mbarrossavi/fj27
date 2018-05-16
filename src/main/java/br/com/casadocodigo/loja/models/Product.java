ce8c0b1c21de1e9a57ec668d92024896523d2176
Marcelo Barros Savi <mbsavi@gmail.com>
2018-05-15 23:05:53 -0300

    segunda aula

---
+++ src/main/java/br/com/casadocodigo/loja/models/Product.java
@@ -0,0 +1,62 @@
+package br.com.casadocodigo.loja.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [title=" + title + ", description=" + description + ", numberOfPages=" + numberOfPages + "]";
+	}
+
+	private String title;
+	
+	@Lob
+	private String description;
+	private int numberOfPages;
+	
+	
+	//getters and setters
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+	
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+	
+
+}
---
+++ src/main/webapp/WEB-INF/views/products/ok.jsp
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Cadastro de livros</title>
+</head>
+
+<body>
+	<p>Produto cadastrado com sucesso</p>
+</body>
+
+
+</html>
